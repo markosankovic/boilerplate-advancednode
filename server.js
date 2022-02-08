7d232639b80f6ee81ff70e2c77f22a63e84cad14
Marko Sanković <sankovic.marko@gmail.com>
2022-02-08 19:18:41 +0100

    Connect to database

--- server.js
+++ server.js
@@ -21,24 +21,32 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
 
-passport.deserializeUser((id, done) => {
-  // myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: 'Connected to Database', message: 'Please login' });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch((e) => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
