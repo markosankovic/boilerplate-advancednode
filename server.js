763f1d84bd215a1b4faf12e4f0ffc0d740bfb746
Marko Sanković <sankovic.marko@gmail.com>
2022-02-08 20:11:37 +0100

    Add logout route and handle 404

--- server.js
+++ server.js
@@ -71,6 +71,15 @@ myDB(async (client) => {
       res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
     }
   );
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
