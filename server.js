36e0b8b1e3e5722db5839733e15f87f6300667d7
Marko Sanković <sankovic.marko@gmail.com>
2022-02-08 14:10:04 +0100

    Add passport and session

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
 app.set('view engine', 'pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
