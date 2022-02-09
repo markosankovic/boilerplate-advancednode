09dc6b3539bce1c31c98b8901a6e7bdcab27ad45
Marko Sanković <sankovic.marko@gmail.com>
2022-02-09 09:11:00 +0100

    Remove configure heroku

--- configure-heroku.sh
+++
@@ -1,5 +0,0 @@
-heroku config:set \
-  MONGO_URI="mongodb+srv://Wemblr:yMk3egUjiblTHgzr@cluster0.rrvwd.mongodb.net/database?retryWrites=true&w=majority" \
-  GITHUB_CLIENT_ID="edb9a06e61ffeafdf8df" \
-  GITHUB_CLIENT_SECRET="171d0faa56ba5e140cd334078c20035db29fd2eb" \
-  GITHUB_CALLBACK_URL="https://fcc-qa-adv-node-and-express.herokuapp.com/auth/github/callback"
