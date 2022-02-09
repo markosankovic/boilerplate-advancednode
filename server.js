9ee0b17f3f5f0cec8f3b2c56ce7673384707827f
Marko Sanković <sankovic.marko@gmail.com>
2022-02-09 07:18:30 +0100

    Encrypt password

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -52,7 +53,9 @@ myDB(async (client) => {
         console.log(`User ${username} attempted to log in.`);
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
         return done(null, user);
       });
     }
@@ -65,9 +68,10 @@ myDB(async (client) => {
       } else if (user) {
         res.redirect('/');
       } else {
+        const hash = bcrypt.hashSync(req.body.password, 12);
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password,
+          password: hash,
         }, (err, doc) => {
           if (err) {
             res.redirect('/');
