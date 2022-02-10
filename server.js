b9128b72dfb4bd79e4b3836ac98a2a43cf4d324d
Marko Sanković <sankovic.marko@gmail.com>
2022-02-10 11:06:08 +0100

    Add user to socket

--- server.js
+++ server.js
@@ -7,16 +7,45 @@ const routes = require('./routes');
 const auth = require('./auth');
 const passport = require('passport');
 const session = require('express-session');
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
+  }),
+);
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 let currentUsers = 0;
 
 io.on('connection', socket => {
-  console.log('A user has connected');
+  console.log('user ' + socket.request.user.name + ' connected');
   ++currentUsers;
   io.emit('user count', currentUsers);
   socket.on('disconnect', () => {
