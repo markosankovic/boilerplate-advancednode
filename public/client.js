c19f01a81ccc3a722d35e1c2e5299ffb1ab4cfee
Marko Sanković <sankovic.marko@gmail.com>
2022-02-10 11:18:16 +0100

    Fix: name of user socket event

--- public/client.js
+++ public/client.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   /* Global io */
   let socket = io();
 
-  socket.on('user count', function (data) {
+  socket.on('user', function (data) {
     $('#num-users').text(data.currentUsers + ' users online');
     let message = data.name + (data.connected ? ' has joined the chat.' : ' has left the chat.');
     $('#messages').append($('<li>').html('<b>' + message + '</b>'));
